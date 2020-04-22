982a302e1b6cf9a20949566135aa4be5d47ff9c0
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-04-22 17:28:56 -0400

    Update Income.sql

--- Case Studies/Project 2/Income.sql
+++ Case Studies/Project 2/Income.sql
@@ -53,4 +53,74 @@ mysql> SELECT * FROM CH2_Rehab_Income;
 
 -------------------------------------------------------------
 
+mysql> CREATE TABLE CH2_Mobile_Income (
+    -> Income_statment VARCHAR(20), 
+    -> In_thousands INT);
+Query OK, 0 rows affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands) 
+    -> VALUES ('Sale Revenue', 5811);
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('COGS', 3411);
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('Gross Profit', 2400);
+Query OK, 1 row affected (0.01 sec)
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('Operating Expenses', null);
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES (' General & Administrative', 292); 
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES (' Sales & Marketing', 1389 );
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES (' Research & Development', 214 );
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES (' Other', 5 );
+Query OK, 1 row affected (0.00 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('Total Operating Expenses', 1900 );
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('Operating Income', 500);
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES (' Interest Expense', 1);
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('Income Before Taxes', 499);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('Taxes', 127);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> INSERT INTO CH2_Mobile_Income (Income_statment, In_thousands)  VALUES ('Net Income', 372);
+Query OK, 1 row affected (0.01 sec)
+
+-------------------------------------------------------------
+mysql> SELECT * FROM CH2_Mobile_Income;
++---------------------------+--------------+
+| Income_statment           | In_thousands |
++---------------------------+--------------+
+| Sale Revenue              |         5811 |
+| COGS                      |         3411 |
+| Gross Profit              |         2400 |
+| Operating Expenses        |         NULL |
+|  General & Administrative |          292 |
+|  Sales & Marketing        |         1389 |
+|  Research & Development   |          214 |
+|  Other                    |            5 |
+| Total Operating Expenses  |         1900 |
+| Operating Income          |          500 |
+|  Interest Expense         |            1 |
+| Income Before Taxes       |          499 |
+| Taxes                     |          127 |
+| Net Income                |          372 |
++---------------------------+--------------+
+14 rows in set (0.00 sec)
+
+-------------------------------------------------------------
 
